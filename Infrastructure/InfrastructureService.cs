223cf63c700ca9280485c0cfd3dc4e6ce8b71a1e
Yugo121 <adam_kaszynski@tlen.pl>
2025-08-07 13:48:57 +0200

    Added comments to switch sql server to sql lite

--- Infrastructure/InfrastructureService.cs
+++ Infrastructure/InfrastructureService.cs
@@ -26,5 +26,33 @@ namespace Infrastructure
             services.AddHttpClient<IBaselinkerService, BaselinkerService>();
             services.AddHttpClient<INuboService, NuboService>();
         }
-    }
+
+        //public InfrastructureService(IServiceCollection services, IConfiguration configuration)
+        //{
+        //    // 1) Connection string do pliku SQLite (zdefiniowany w appsettings.json)
+        //    var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        //    // 2) Rejestrujemy DbContextFactory z providerem SQLite
+        //    services.AddDbContextFactory<AppDbContext>(options =>
+        //        options.UseSqlite(connectionString)
+        //               .EnableSensitiveDataLogging()
+        //    );
+
+        //    // 3) Rejestrujemy IAppDbContext tak jak wcześniej
+        //    services.AddScoped<IAppDbContext>(sp =>
+        //        sp.GetRequiredService<IDbContextFactory<AppDbContext>>().CreateDbContext()
+        //    );
+
+        //    // … reszta Twoich serwisów bez zmian
+        //    services.AddScoped<ICsvProductParser, CsvProductParser>();
+        //    services.AddHttpClient<IBaselinkerService, BaselinkerService>();
+        //    services.AddHttpClient<INuboService, NuboService>();
+        //} - to jest do SQLite, w domu usunąć sql server, dodać paczkę nugetową z litem, na froncie dodać electrona i sprawdzić czy bedzie działać apka
+
+//        {
+//  "ConnectionStrings": {
+//    "DefaultConnection": "Data Source=appdata.db"
+//  }
+//} - to jeszcze do appsettings wrzucić
+}
 }
